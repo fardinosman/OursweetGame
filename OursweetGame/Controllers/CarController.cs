1764556f65ab2c97a12ddcb11448516a89bbb784
Fardin Osman <osmanfardin@hotmail.dk>
2019-10-07 10:17:28 +0200

    Car controller

---
+++ OursweetGame/Controllers/CarController.cs
@@ -0,0 +1,35 @@
+﻿using OursweetGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OursweetGame.Controllers
+{
+    public class CarController : Controller
+    {
+        // GET: Car
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CreateCar()
+        {
+            Car car = new Car();
+
+            
+            return View(car);
+        }
+        [HttpPost]
+        public ActionResult CreateCar(string Color, string Make, int Year)
+        {
+            Car car = new Car();
+
+            car.Color = Color;
+            car.Make = Make;
+            car.Year = Year;
+            return null;
+        }
+    }
+}
---
+++ OursweetGame/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OursweetGame.Models
+{
+    public class Car
+    {
+
+        public string Color { get; set; }
+        public string Make { get; set; }
+        public int Year { get; set; }
+    }
+}
